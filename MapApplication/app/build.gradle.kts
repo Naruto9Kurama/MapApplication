32b51302601bf59d2be027f25941bb84dfd4023b
Me <1318094164@qq.com>
2023-12-09 19:27:27 +0800

    添加osmdroid依赖，并成功显示出地图

--- MapApplication/app/build.gradle.kts
+++ MapApplication/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.creator.mapapplication"
-        minSdk = 33
+        minSdk = 31
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -58,4 +58,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //osmdroid
+    implementation("org.osmdroid:osmdroid-android:6.1.17")
+
 }
--- MapApplication/app/src/main/java/com/creator/mapapplication/MainActivity.kt
+++ MapApplication/app/src/main/java/com/creator/mapapplication/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.creator.mapapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
-import android.widget.TextView
+import android.preference.PreferenceManager
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.creator.mapapplication.databinding.ActivityMainBinding
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +18,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+// Configure osmdroid
+        // Configure osmdroid
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
+
+        // Initialize the map view
+
+        // Initialize the map view
+        var mapView = binding.mapView
+        mapView.setTileSource(TileSourceFactory.OpenTopo) // You can choose different tile sources
+
+        mapView.setBuiltInZoomControls(true)
+
+        // Set initial map center
+
+        // Set initial map center
+        val startPoint = GeoPoint(0, 0) // Set your initial coordinates
+
+        mapView.getController().setCenter(startPoint)
+        mapView.getController().setZoom(10) // Se
+
 
     }
 
@@ -19,12 +47,12 @@ class MainActivity : AppCompatActivity() {
      * A native method that is implemented by the 'mapapplication' native library,
      * which is packaged with this application.
      */
-/*    external fun stringFromJNI(): String
+    external fun stringFromJNI(): String
 
     companion object {
         // Used to load the 'mapapplication' library on application startup.
         init {
             System.loadLibrary("mapapplication")
         }
-    }*/
+    }
 }
